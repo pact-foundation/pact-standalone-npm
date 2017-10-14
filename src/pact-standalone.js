10dc94a07809638828ba55c66b9db9d938b19bee
Michel Boudreau <michelboudreau@gmail.com>
2017-10-14 12:25:39 +1100

    fixing cwd

--- src/pact-standalone.js
+++ src/pact-standalone.js
@@ -3,14 +3,14 @@ var system = process.platform + (process.platform === 'linux' ? '-' + process.ar
 var basePath = "platforms/" + system + '/bin/';
 var isWindows = process.platform === 'win32';
 
-var cwd = path.resolve(__dirname, '..', basePath);
+var cwd = path.resolve(__dirname, '..');
 var mockService = ('pact-mock-service' + (isWindows ? '.bat' : ''));
 var verifier = ('pact-provider-verifier' + (isWindows ? '.bat' : ''));
 
 module.exports = {
 	cwd: cwd,
-	mockServicePath: basePath + mockService,
-	mockServiceFullPath: path.resolve(cwd, mockService).trim(),
-	verifierPath: basePath + verifier,
-	verifierFullPath: path.resolve(cwd, verifier).trim()
+	mockServicePath: path.join(basePath, mockService),
+	mockServiceFullPath: path.resolve(cwd, basePath, mockService).trim(),
+	verifierPath: path.join(basePath, verifier),
+	verifierFullPath: path.resolve(cwd, basePath, verifier).trim()
 };
